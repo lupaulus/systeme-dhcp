8c724e1eabbb215b44e70beb4545a5c6c9fd3721
Lucas PAULUS <lucas67500@gmail.com>
2020-11-19 19:19:40 +0100

    modification

--- git-server/git-create-repo.sh
+++ git-server/git-create-repo.sh
@@ -7,8 +7,10 @@ mkdir -p "${GIT_DIR}/${REPO_NAME}.git"
 cd "${GIT_DIR}/${REPO_NAME}.git"
  
 git init --bare &> /dev/null
-# touch git-daemon-export-ok
+
 cp hooks/post-update.sample hooks/post-update
+echo "#!/bin/sh \
+curl http://172.20.0.3/jenkins/generic-webhook-trigger/invokehooks" > hooks/post-update
 git update-server-info
 chmod 777 -R "${GIT_DIR}/${REPO_NAME}.git"
 chown -Rf www-data:www-data "${GIT_DIR}/${REPO_NAME}.git"
