01387cf3f60a3872b971dceaef0beef66dd4096e
Yoann Walch <ywalch@cfai-formation.fr>
2021-01-10 20:25:26 +0100

    tp04 entier

--- tp04/proposer_un_service/docker_jenkins.sh
+++ tp04/proposer_un_service/docker_jenkins.sh
@@ -1,13 +1,7 @@
-id_conteneur=$(docker run --name jenkins-master -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home jenkins/jenkins:lts);
+mkdir -p jenkins
+cd jenkins
+id_conteneur=$(docker run --name jenkins-master -d -p 8080:8080 -p 50000:50000 -v $(pwd):/var/jenkins_home jenkins/jenkins:lts);
 docker logs $id_conteneur;
-secs=25
-echo "pour cause de préparation, on ouvre jenkins dans:"
-while [ $secs -gt 0 ]
-do
-  printf "$secs...\n"
-  secs=$((secs-1))
-  sleep 1
-done
-echo "LEROOOOOOOOOOOOY JENKINS !";
+echo "Ouvrons JENKINS !";
 sleep 1;
 firefox http://127.0.0.1:8080;
---
+++ tp04/proposer_un_service/jenkins/.bash_history
@@ -0,0 +1,10 @@
+cat /var
+cat /var/jenkins_home/secrets/initialAdminPassword
+docker rm -vf $(docker ps -a -q)
+exit
+cat /var/jenkins_home/secrets/initialAdminPassword
+exit
+cat /var/jenkins_home/secrets/initialAdminPassword
+exit
+cat /var/jenkins_home/secrets/initialAdminPassword
+exit
