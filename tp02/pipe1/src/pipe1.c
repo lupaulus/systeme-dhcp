7a9627801ea301653653f6c4ffa5c9f0f7a85d64
baptiste.steinmetz <baptiste.steinmetz67@gmail.com>
2021-01-14 10:54:37 +0100

    added correct 3rd exercice tp02

--- tp02/pipe1/src/pipe1.c
+++ tp02/pipe1/src/pipe1.c
@@ -1,66 +1,48 @@
-/**
- * \file skeleton.c
- * \brief Basic parsing options skeleton.
- * \author Pierre L. <pierre1.leroy@orange.com>
- * \version 0.1
- * \date 10 septembre 2016
- *
- * Basic parsing options skeleton exemple c file.
- */
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-
-#include <getopt.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/fcntl.h>
 #include <sys/wait.h>
 
-
-
-
-
-#define STDOUT 1
-#define STDERR 2
-
-#define MAX_PATH_LENGTH 4096
-
-
-
-
-/**
- * Procedure which displays binary usage
- * by printing on stdout all available options
- *
- * \return void
- */
-void print_usage(char* bin_name)
-{
-dprintf(1, "USAGE: %s\n", bin_name);
-}
-
-
-
-
-
-/**
- * Binary main loop
- *
- * \return 1 if it exit successfully 
- */
 int main(int argc, char** argv)
 {
+	// tab contenant les deux file descriptor
+	int pipedes[2];
 
-  if(argc != 2)
-  {
-    print_usage(argv[0]);
-    return EXIT_FAILURE;
-  }
+	// réalisation de la connection entre deux process
+	pipe(pipedes);
 
-  printf("Arg 1 : %s\n", argv[1]);
+	//exécution de la première commande	
+	if (fork() == 0) {
+		close(pipedes[0]);
+		dup2(pipedes[1], 1);
+		close(pipedes[1]);
 
-  return EXIT_SUCCESS;
+        // première partie de la commande ' ps eaux ' 
+		execlp ("ps", "ps", "eaux", (void*)0);
+		exit(1);
+	}
+	
+	// exécution de la deuxième commande
+	if (fork() == 0 ) {
+		close(pipedes[1]);
+		dup2(pipedes[0], 0);
+
+		// On crée la redirection 
+		int fd = open("/dev/null", O_WRONLY);
+		dup2(fd, 1);
+		// Deuxième partie ' | grep "^root"  '
+		execlp ("grep", "grep", "^root", (void*)0);
+
+		exit(1);
+	}
+
+	close(pipedes[1]);
+	close(pipedes[0]);
+
+	wait(&pipedes[1]);
+	wait(&pipedes[0]);
+
+	write(1, "root est connecté\n", 20);
+	return 0; 
 }
